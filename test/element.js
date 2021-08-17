6f5bc26097dacd70c9314174997a2131690f9da9
Eseoghene Agadagba <61781464+ese-aga@users.noreply.github.com>
2021-08-17 09:52:18 +0100

    Update element.js

--- test/element.js
+++ test/element.js
@@ -1,24 +1,28 @@
-describe("Should be able to access the Volvo campaign page", function() {
-    it("Accesss Volvo a-million-more page", function() {
+describe("Should be able to assess the Volvo Campaign Page", () => {
+
+    it("Accesss Volvo a-million-more page", () => {
+        // Assert landing page
         browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more');
-        const search = $('#IconCallouts-1');
-        const text = search.getText();
+        const text = $('#IconCallouts-1');
+        const langingPage = text.getText();
 
         const cookie = $("//button[@aria-label='Accept']");
         cookie.click();
     
     });
 
-    it("Find Volvo XC90 description page", function() {
-       const ourCar = $("//span[text()='Our Cars']");
-       ourCar.click();
+    it("Should be able to find Volvo V90 description page", () => {
+        //Open "Our Car" on Volvo a-million-more page
+        const ourCar = $("//span[text()='Our Cars']");
+        ourCar.click();
+
+        //Navigate to V90 Recharge description page
+        const nav = $('//*[@id="site-nav-cars-menu-section-panel-1"]/div[5]/a/div/div/div[1]');
+        nav.click();
         
-       const nav = $("._SiteNav-mw [href='/intl/v/cars/v90-hybrid'] > ._SiteNav-ag > ._SiteNav-ag");
-       nav.click();
-
-       const xc90HeaderTitle = $("[data-autoid='pdpsubmenu:nav-desktop']");
-       expect(xc90HeaderTitle).toBeDisplayed();
-
-    });
+        // Assert we are on V90 Recharge Description page
+        const V90HeaderTitle = $('//*[@id="PdpSubmenu-1"]/div[2]/div/section/div[1]/div[1]/h1');
+        expect(V90HeaderTitle).toHaveTextContaining("Volvo V90 Recharge");
+ 
+     });
 });
-
