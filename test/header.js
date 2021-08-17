09f5fa53e109ea653c7e67d7132828d5320cdd34
Eseoghene Agadagba <61781464+ese-aga@users.noreply.github.com>
2021-08-17 09:52:56 +0100

    Update header.js

--- test/header.js
+++ test/header.js
@@ -1,9 +1,11 @@
-describe("interaction with web element", function() {
-    it("get header value", function() {
+describe("Should be able to interaction with web element", () => {
+    it("get header value", () => {
+
+        //Select Campaign Heading "Ideas that change the world are often the most controversial."
+        
         browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more');
         const header = $('#ModelIntro-1');
-        let text = header.getText();
+        const text = header.getText();
         console.log(text);
     });
 });
-
